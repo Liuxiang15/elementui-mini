3495df73282ac945d79af470e577240b6ec8e8f8
liuxiang7 <liuxiang7@tal.com>
2023-04-08 22:20:01 +0800

    feat:col和row实现基础布局

---
+++ src/components/element/Row.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="el-row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+.el-row {
+  &::after,
+  &::before {
+    display: block;
+    content: '';
+  }
+  &::after {
+    clear: both;
+  }
+}
+</style>
