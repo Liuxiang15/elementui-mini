af543a378819a93411a3d53693bec143fc3a6888
liuxiang7 <liuxiang7@tal.com>
2023-04-08 22:42:17 +0800

    feat:分栏间隔

--- src/components/element/Col.vue
+++ src/components/element/Col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['el-col', `el-col-${span}`]">
+  <div :class="['el-col', `el-col-${span}`]" :style="style">
     <slot></slot>
   </div>
 </template>
@@ -12,17 +12,34 @@ export default {
       default: 24,
     },
   },
+  computed: {
+    gutter() {
+      let parent = this.$parent;
+      while (parent && parent.$options.name !== 'ElRow') {
+        parent = parent.$parent;
+      }
+      return parent ? parent.gutter : 0;
+    },
+    style() {
+      const style = {};
+      if (!this.gutter) {
+        return style;
+      }
+      style.paddingLeft = style.paddingRight = `${this.gutter / 2}px`;
+      return style;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 .el-col {
-    float: left;
+  float: left;
+  box-sizing: border-box;
 }
 
 @for $i from 1 through 24 {
-    .el-col-#{$i}{
-        width: $i / 24 * 100%;
-    }
+  .el-col-#{$i} {
+    width: $i / 24 * 100%;
+  }
 }
-
 </style>
--- src/components/element/Row.vue
+++ src/components/element/Row.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="el-row">
+  <div class="el-row" :style="style">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'ElRow',
+  props: {
+    gutter: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    style() {
+      const style = {};
+      if (this.gutter) {
+        style.marginLeft = `${-this.gutter / 2}px`;
+        style.marginRight = style.marginLeft;
+      }
+      return style;
+    },
+  },
 };
 </script>
 
