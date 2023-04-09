c1e06ba44867675a817c25265d7b6ced5bb0de33
liuxiang7 <liuxiang7@tal.com>
2023-04-09 17:07:38 +0800

    refactor:引入

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
 import Vue from 'vue';
 
-
-import '@/assets/theme-chalk/display.scss';
-import '@/assets/theme-chalk/index.scss';
-
-// 2、按需引入
-import Row from '@element/Row/index.js';
-import Col from '@element/Col/index.js';
 import App from './App.vue';
 
-Vue.use(Row);
-Vue.use(Col);
+import './plugins/element.js';
 
-// 1、完整引入
-// import element from '@element/index.js';
-
-// Vue.use(element);
 Vue.config.productionTip = false;
 
 new Vue({
