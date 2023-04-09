0261f6111a11637a59d6abc410991b5d1e08a1b7
liuxiang7 <liuxiang7@tal.com>
2023-04-09 11:19:04 +0800

    feat:响应式布局

--- src/components/element/Col.vue
+++ src/components/element/Col.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    :class="['el-col', classList]"
-    :style="style"
-  >
+  <div :class="['el-col', classList]" :style="style">
     <slot></slot>
   </div>
 </template>
@@ -26,6 +23,11 @@ export default {
       type: Number,
       default: 0,
     },
+    xs: [Number, Object],
+    sm: [Number, Object],
+    md: [Number, Object],
+    lg: [Number, Object],
+    xl: [Number, Object],
   },
   computed: {
     gutter() {
@@ -50,6 +52,30 @@ export default {
         if (!this[prop]) return;
         const className = prop === 'span' ? `el-col-${this[prop]}` : `el-col-${prop}-${this[prop]}`;
         classList.push(className);
+      });
+      const sizeList = ['xs', 'sm', 'md', 'lg', 'xl'];
+      sizeList.forEach((size) => {
+        if (typeof this[size] === 'number') {
+          classList.push(`el-col-${size}-${this[size]}`);
+        } else if (typeof this[size] === 'object') {
+          //  参数为对象类型实例
+          // {
+          //   span: 6,
+          //   offset: 8,
+          //   push: 7,
+          //   pull: 9
+          // }
+          // el-col-xs-6
+          // el-col-xs-offset-8
+          // el-col-xs-push-7
+          // el-col-xs-pull-9
+          const props = this[size];
+          Object.keys(props).forEach((prop) => {
+            const className = prop === 'span' ? `el-col-${size}-${props[prop]}`
+              : `el-col-${size}-${prop}-${props[prop]}`;
+            classList.push(className);
+          });
+        }
       });
       return classList;
     },
@@ -80,4 +106,144 @@ export default {
     right: $i / 24 * 100%;
   }
 }
+
+// xs < 760px
+@media only screen and (max-width: 768px - 1px) {
+  @for $i from 0 through 24 {
+    .el-col-xs-0 {
+      display: none;
+    }
+
+    .el-col-xs-#{$i} {
+      width: $i / 24 * 100%;
+    }
+
+    .el-col-xs-offset-#{$i} {
+      margin-left: $i / 24 * 100%;
+    }
+
+    .el-col-xs-push-#{$i} {
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+
+    .el-col-xs-pull-#{$i} {
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+// sm >= 768px
+@media only screen and (min-width: 768px){
+  @for $i from 0 through 24 {
+    .el-col-sm-0 {
+      display: none;
+    }
+
+    .el-col-sm-#{$i} {
+      width: $i / 24 * 100%;
+    }
+
+    .el-col-sm-offset-#{$i} {
+      margin-left: $i / 24 * 100%;
+    }
+
+    .el-col-sm-push-#{$i} {
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+
+    .el-col-sm-pull-#{$i} {
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+
+// md >= 992px
+@media only screen and (min-width: 992px){
+  @for $i from 0 through 24 {
+    .el-col-md-0 {
+      display: none;
+    }
+
+    .el-col-md-#{$i} {
+      width: $i / 24 * 100%;
+    }
+
+    .el-col-md-offset-#{$i} {
+      margin-left: $i / 24 * 100%;
+    }
+
+    .el-col-md-push-#{$i} {
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+
+    .el-col-md-pull-#{$i} {
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+
+// lg >= 1200px
+@media only screen and (min-width: 1200px){
+  @for $i from 0 through 24 {
+    .el-col-lg-0 {
+      display: none;
+    }
+
+    .el-col-lg-#{$i} {
+      width: $i / 24 * 100%;
+    }
+
+    .el-col-lg-offset-#{$i} {
+      margin-left: $i / 24 * 100%;
+    }
+
+    .el-col-lg-push-#{$i} {
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+
+    .el-col-lg-pull-#{$i} {
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+
+// xl >= 1920px
+@media only screen and (min-width: 1920px){
+  @for $i from 0 through 24 {
+    .el-col-xl-0 {
+      display: none;
+    }
+
+    .el-col-xl-#{$i} {
+      width: $i / 24 * 100%;
+    }
+
+    .el-col-xl-offset-#{$i} {
+      margin-left: $i / 24 * 100%;
+    }
+
+    .el-col-xl-push-#{$i} {
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+
+    .el-col-xl-pull-#{$i} {
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+
 </style>
