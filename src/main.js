83ceea6c30de9b8092918add2bc214f1b3c3d023
liuxiang7 <liuxiang7@tal.com>
2023-04-09 17:01:50 +0800

    feat:支持全局引入和按需引入

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
 
+
 import '@/assets/theme-chalk/display.scss';
 import '@/assets/theme-chalk/index.scss';
 
+// 2、按需引入
 import Row from '@element/Row/index.js';
 import Col from '@element/Col/index.js';
 import App from './App.vue';
 
 Vue.use(Row);
 Vue.use(Col);
+
+// 1、完整引入
+// import element from '@element/index.js';
+
+// Vue.use(element);
 Vue.config.productionTip = false;
 
 new Vue({
