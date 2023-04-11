d4089143e35edd1abd7e7b640554326045fa2fdc
liuxiang7 <liuxiang7@tal.com>
2023-04-11 22:21:33 +0800

    test:容器测试组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <RowTest />
+    <!-- <RowTest /> -->
+    <ContainerTest/>
   </div>
 </template>
 
 <script>
 import RowTest from '@element/Row/test';
+import ContainerTest from '@element/container/test';
 
 export default {
   name: 'app',
   components: {
     RowTest,
+    ContainerTest,
   },
 };
 </script>
