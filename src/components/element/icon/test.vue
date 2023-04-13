4b6344e7c70097c4ca22a3cdab783e8924fee133
liuxiang7 <liuxiang7@tal.com>
2023-04-13 22:54:02 +0800

    feat:icon

---
+++ src/components/element/icon/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-icon name="eleme"></el-icon>
+    <el-icon name="loading"></el-icon>
+    <i class="el-icon-edit"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
